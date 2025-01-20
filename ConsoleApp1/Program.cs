fb8c9d3d0074b0caeae4bffa66cbfde32c31bc42
trenf <trenf@DESKTOP-JB5DFJ5>
2025-01-19 20:34:00 -0600

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exc1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your first name?");//to get first name
+            string firstName = Console.ReadLine()//to get first name
+            Console.WriteLine("What is your last name?");//to get last name
+            string lastName = Console.ReadLine();//to get last name
+            Console.WriteLine("What is your age?");//to get age
+            string age = Console.ReadLine();//to get age
+            Console.WriteLine("What is your favorite color?");//to get colors
+            string favColor = Console.ReadLine();//to get colors
+            Console.WriteLine("Hello " + firstName + " " + lastName + " you are " + age + " and your favorite color is " + favColor + ".");//final output
+        }
+    }
+}
